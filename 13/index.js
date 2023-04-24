d0d9d815a90a162490b91bb31a549587ab2b42fb
janay <janayby@gmail.com>
2023-04-24 10:57:14 +0300

    add challenge 13

---
+++ 13/index.js
@@ -0,0 +1,45 @@
+
+/* 
+Scrimba mascot Pumpkin has won the grand prize at an international 
+cat show. Below are Pumpkin's scores from the judges, as well as all the 
+prizes he's won. In all the excitement of victory,
+they've become a jumbled mess of nested arrays. Let's 
+help Pumpkin by sorting it out. 
+
+Write a function to flatten nested arrays of strings or
+numbers into a single array. There's a method
+for this, but pratice both doing it manually and using the method. 
+
+Example input: [1, [4,5], [4,7,6,4], 3, 5]
+Example output: [1, 4, 5, 4, 7, 6, 4, 3, 5]
+*/
+
+const kittyScores = [
+    [39, 99, 76], 89, 98, [87, 56, 90], 
+    [96, 95], 40, 78, 50, [63]
+];
+
+const kittyPrizes = [
+    ["💰", "🐟", "🐟"], "🏆", "💐", "💵", ["💵", "🏆"],
+    ["🐟","💐", "💐"], "💵", "💵", ["🐟"], "🐟"
+];
+
+function flatten(arr){
+    // // the shortest way
+    // return arr.flat()
+
+    output = []
+    
+    // the long way
+    arr.forEach(element => {
+        if(Array.isArray(element)) {
+            element.forEach(item => output.push(item))
+        } else {
+            output.push(element)
+        }
+    });
+    return output
+}
+
+console.log(flatten(kittyPrizes));
+// console.log(flatten(kittyScores));
