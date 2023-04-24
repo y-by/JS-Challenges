c8e2a18e993106bbbeeec1e4e5afa37eb6419070
janay <janayby@gmail.com>
2023-04-24 14:45:53 +0300

    add challenge 14

---
+++ 14/index.js
@@ -0,0 +1,33 @@
+/*
+Alex from Scrimba wants to know how many new students have attended 
+Scrimba's weekly Town Hall event this year. 
+
+He has an array of first-time attendees for each month of the year. 
+Help him find the total number of attendees! Your function should
+take in an array and return a number representing the total number
+of new attendees. 
+
+Example input: [1,2,3]
+Example output: 6
+ */
+
+const studentCount = [50,53,61,67,60,70,78,80,80,81,90,110];
+
+function sumArray(arr){
+    // short solution with .reduce()
+    const sumValues =  arr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue, 0
+    )
+    return sumValues
+    
+    let sum = 0
+    // // one way
+    // for (let i = 0; i < arr.length; i++) {
+    //     sum += arr[i];
+    // }
+    // // second way
+    // arr.forEach(item => sum += item);
+    return sum
+}
+
+console.log(sumArray(studentCount));
